c56794a1caa3368e65632b87b483f1701ab8f8f4
Abdulbasit <habdulbasit099@gmail.com>
2023-12-06 16:50:42 +0200

    fixed

---
+++ app/api/[storeId]/billboards/[billboardId]/route.tsx
@@ -0,0 +1,87 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!label) {
+      new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.billboardId) {
+      new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.updateMany({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[STORE_PATCH]", error);
+    new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE]", error);
+    new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./button";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
+interface ImageUploadProps {
+  disabled: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  onRemove,
+  value,
+  disabled,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="md-4 flex items-center gap-4">
+        {value.map((url) => {
+          return (
+            <div
+              key={url}
+              className="relative w-[300px] h-[200px] rounded-md overflow-hidden"
+            >
+              <div className="z-10 absolute top-2 right-2">
+                <Button
+                  type="button"
+                  variant="destructive"
+                  onClick={() => onRemove(url)}
+                >
+                  <Trash className="h-4 w-4" />
+                </Button>
+              </div>
+              <Image fill className="object-cover" alt="image" src={url} />
+            </div>
+          );
+        })}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="zsoxpruw">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+
+          return (
+            <Button
+              type="button"
+              disabled={disabled}
+              variant="secondary"
+              onClick={onClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
+
+export default ImageUpload;
